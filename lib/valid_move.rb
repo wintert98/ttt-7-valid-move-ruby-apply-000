1975a23570047d14f2f104522895074cd28aedfd
Thomas Winter <wintert98@gmail.com>
2020-10-01 18:05:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
    !position_taken?(board, index) && (index).between?(1,9)
  
-  end
+  
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
