df296b229abe84c8a72b85a0d6bc62f96f9bc7c4
Thomas Winter <wintert98@gmail.com>
2020-10-01 16:18:56 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 # code your #valid_move? method here
-
+def 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
