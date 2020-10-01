d8d0180fbd3f6819f198aabdcfdfdbe1168777d4
Thomas Winter <wintert98@gmail.com>
2020-10-01 17:08:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,8 +6,7 @@ def valid_move?(board, index)
     return true
   elsif position_taken?(board, index) == true
     return false
-    else index.between?(1,9) && !position_taken?(board, index)
-      return false
+    
   end
 end
 
